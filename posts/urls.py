aeee2decbe4910cc189f3f4ad9329010437d6913
ndodaj <floriandodaj55@gmail.com>
2020-12-13 18:05:48 +0100

    Added the website navbar; PostDetail and  Addpost view and some styling

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .views import HomeView
+from .views import HomeView, PostDetail, AddPost
 from django.urls import path
 
 urlpatterns = [
-   path('', HomeView.as_view(), name="home ")
+   path('', HomeView.as_view(), name='home'),
+   path('posts/<int:pk>', PostDetail.as_view(), name='post-detail'),
+   path('add_post/', AddPost.as_view(), name='add_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView # ListView - Allows me to list a query set into the database, detailView - brings one record only
+from django.views.generic import ListView, DetailView , CreateView # ListView - Allows me to list a query set into the database, detailView - brings one record only
 from .models import Post
-# def home(request):
-#     return render(request, 'home.html', {})
 
 #class views
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class AddPost(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__' # allows me to put all as fileds
+
